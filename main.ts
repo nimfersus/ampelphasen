1ef5ffbd4bdd70e834e0b703edb19e982f49cd97
nimfersus <105855729+nimfersus@users.noreply.github.com>
2022-05-19 15:41:48 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,17 @@
 input.onButtonPressed(Button.A, function () {
     wunsch = 1
 })
-let Farbe = 0
 let wunsch = 0
 wunsch = 0
+let Farbe = 0
 basic.forever(function () {
     if (Farbe == 1) {
+        basic.setLedColor(0x00ff00)
         basic.pause(9000)
         Farbe = 0
         wunsch = 0
+    } else {
+        basic.setLedColor(0xff0000)
     }
     if (wunsch && Farbe == 0) {
         basic.showString("Grün kommt.")
